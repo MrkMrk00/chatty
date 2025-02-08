8aec828e2f46096fed65a8f1ec2fd2fb5e697099
Marek Hauschwitz <hauschwitz@wpj.cz>
2025-02-08 14:16:45 +0100

    lint fix

--- utils/cn.ts
+++ utils/cn.ts
@@ -1,5 +1,5 @@
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { clsx, type ClassValue } from "clsx";
 
 export function cn(...classValues: ClassValue[]): string {
     return twMerge(clsx(...classValues));
