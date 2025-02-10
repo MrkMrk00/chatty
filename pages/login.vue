88472c8a3512053564ea527e30c75108ebbe85cd
MrkMrk00 <mhau@outlook.cz>
2025-02-10 18:22:13 +0100

    functional login form

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,72 @@
 <script setup lang="ts">
 import Input from "~/components/Input.vue";
+import { loginValidator } from '~/utils/auth';
+
+function getEmptyErrorState() {
+    return {
+        email: [] as string[],
+        password: [] as string[],
+    };
+}
+
+const errors = ref(getEmptyErrorState());
+const formRef = useTemplateRef('formRef');
+
+function validateForm() {
+    if (!formRef.value) {
+        return null;
+    }
+
+    const formData = Object.fromEntries(new FormData(formRef.value));
+    const result = loginValidator.safeParse(formData);
+
+    // clear the errors
+    errors.value = getEmptyErrorState();
+
+    if (result.success) {
+        return result.data;
+    }
+
+    for (const err of result.error.errors) {
+        for (const key in errors.value) {
+            if (!err.path.includes(key)) {
+                continue;
+            }
+
+            errors.value[key as keyof typeof errors.value]?.push(err.message);
+        }
+    }
+
+    return null;
+}
+
+function handleLogin() {
+    const formData = validateForm();
+
+    if (!formData) {
+        return;
+    }
+
+
+    const req = new Request('/api/login', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: { 
+            'Content-Type': 'application/json',
+        },
+    });
+
+    fetch(req).then(r => {
+        if (r.ok) {
+            navigateTo('/');
+        }
+    }).catch(err => alert(`Unexpected error ${err}`));
+}
+
+function hasErrors() {
+    return !errors.value.email.length 
+        && !errors.value.password.length;
+}
 </script>
 
 <template>
@@ -9,15 +76,29 @@ import Input from "~/components/Input.vue";
             <h2 class="text-lg">A Chat Bot That Looks as Good as Your Terminal.</h2>
         </div>
         <hr class="border-black dark:border-current">
-        <form action="/api/login" method="post" class="flex flex-col gap-4">
+        <form 
+            action="/api/login" 
+            method="post" 
+            class="flex flex-col gap-4" 
+            @submit.prevent="handleLogin"
+            @change="() => hasErrors() || validateForm()"
+            ref="formRef"
+            novalidate
+        >
             Already have an account?
             <label>
                 Email:
                 <Input name="email" type="email" placeholder="john.doe@outlook.com" required />
+                <span v-for="errorMessage in errors.email" class="text-red-500">
+                    {{ errorMessage }}<br>
+                </span>
             </label>
             <label>
                 Password:
                 <Input name="password" type="password" placeholder="strongpassword" required />
+                <span v-for="errorMessage in errors.password" class="text-red-500">
+                    {{ errorMessage }}<br>
+                </span>
             </label>
 
             <div class="flex justify-end">
--- utils/auth.ts
+++ utils/auth.ts
@@ -6,7 +6,7 @@ export const loginValidator = z.object({
     email: z.string().email(),
     password: z
         .string()
-        .min(6)
+        .min(6, { message: 'Password must be at least 6 characters long.' })
         .refine((pwd) => /[A-Z]/.test(pwd), { message: "Password must contain at least 1 uppercase letter." })
         .refine((pwd) => /[a-z]/.test(pwd), { message: "Password must contain at least 1 lowercase letter." })
         .refine((pwd) => /[0-9]/.test(pwd), { message: "Password must contain at least 1 digit." }),
